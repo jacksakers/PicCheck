a1c645b0a82b5fa510f61b2bf82abb9040104067
Noah Robertson <noahhr@email.sc.edu>
2024-04-23 17:17:12 -0400

    Updated Reminder.kt

--- app/src/main/java/com/example/piccheck/Reminder.kt
+++ app/src/main/java/com/example/piccheck/Reminder.kt
@@ -6,7 +6,7 @@ import androidx.room.*
 
 @Entity
 data class Reminder(
-    @PrimaryKey val id: Int,
+    @PrimaryKey(autoGenerate = true)
     val goal: String,
     val date: String? = null,
     val reflection: String? = null,
